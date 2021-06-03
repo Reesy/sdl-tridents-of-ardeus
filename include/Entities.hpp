f6ad3c20edae3b5f168524a7eae720db10cedd9b
James Rees <James.Rees@outlook.com>
2021-06-03 14:50:22 +0100

    Added first real test case which tests entity creation

--- include/Entities.hpp
+++ include/Entities.hpp
@@ -9,6 +9,4 @@ class Entities
     GameEntity* createBall(SDL_Texture* _texture);
     GameEntity* createLevel(SDL_Texture* _texture);
     GameEntity* createPlayer(SDL_Texture* _texture);
-    
-
 };
--- test/spec/entities/PlayerTest.cpp
+++ test/spec/entities/PlayerTest.cpp
@@ -1,7 +1,13 @@
 #include "../../deps/catch_amalgamated.hpp"
-
+#include <Entities.hpp>
 
 TEST_CASE("Addition test") 
 {
     REQUIRE( 1 == 1);
+}
+
+TEST_CASE("It should be possible to create a player entity")
+{
+    Entities* testEntities = new Entities();
+    REQUIRE( testEntities == NULL);
 }
